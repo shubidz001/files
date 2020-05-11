66e06d38a64f439f700d0a62cd5300a32a194ee4
shubidz001 <33992081+shubidz001@users.noreply.github.com>
2020-05-11 07:59:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
-apt-get update && apt-get upgrade ;
-apt-get install unzip ;
-apt-get install wget ;
-wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/gcp.sh && wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/gcp1.sh
-chmod +x gcp.sh && chmod +x gcp1.sh
-./gcp.sh
+apt-get update; apt-get upgrade -y; ap-get dist-upgrade -y
+apt-get install wget; && apt-get install unzip;
+wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/gcp.sh 
+wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/gcp1.sh
 wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/proftpd.sh
 wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/hlds.sh 
 wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/rehlds.sh
-chmod +x proftpd.sh && chmod +x hlds.sh  && chmod +x rehlds.sh
+chmod +x gcp.sh
+chmod +x gcp1.sh
+chmod +x proftpd.sh
+chmod +x hlds.sh
+chmod +x rehlds.sh
+./gcp.sh
+./gcp1.sh
 ./proftpd.sh
 ./hlds.sh
 ./rehlds.sh
-cd
-   echo "Powered by: shubidz."
-	 echo "Done. Bye!"
