d58cf7f7194ccf5705e71edbc17e888ce16d4af4
shubidz001 <33992081+shubidz001@users.noreply.github.com>
2020-05-11 07:17:05 -0700

    Update h2.sh

--- h2.sh
+++ h2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/shubidz001/files/blob/master/h1.sh
+wget https://github.com/shubidz001/files/blob/shubidz001-patch-1/h1.sh
 chmod +x h1.sh
 ./h1.sh gamecp1 gamecp1
 echo "Thank You."
