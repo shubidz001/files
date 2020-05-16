5b1b55bcdad6e4df0a6aa9449090744ea23aa805
shubidz001 <33992081+shubidz001@users.noreply.github.com>
2020-05-16 04:27:57 -0700

    Delete proftpd.sh

--- proftpd.sh
+++
@@ -1,8 +0,0 @@
-cd /etc/proftpd
-rm -rf proftpd.conf
-wget https://raw.githubusercontent.com/shubidz001/files/shubidz001-patch-1/proftpd.conf
-service proftpd stop
-service proftpd start
-cd
-   echo "Powered by: shubidz."
-	 echo "Done. Bye!"
